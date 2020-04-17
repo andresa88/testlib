0ba10d96f4d3d20f57af2467993655fb1c23d381
Andres Alva <andresa88@gmail.com>
2020-04-17 10:39:33 -0700

    update

---
+++ scripts/prepack.js
@@ -0,0 +1,7 @@
+
+
+const fs = require('fs');
+
+fs.writeFileSync('prepack-out.txt', 'prepack');
+fs.writeFileSync('prepack-inc.txt', 'prepack');
+console.log('prepack');
---
+++ scripts/prepare.js
@@ -0,0 +1,7 @@
+
+
+const fs = require('fs');
+
+fs.writeFileSync('prepare-out.txt', 'prepare');
+fs.writeFileSync('prepare-inc.txt', 'prepare');
+console.log('prepare');
